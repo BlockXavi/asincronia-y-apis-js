8ba677b32c9423945a6f9850e22950e8600c5b6c
XaviBarragan <xbarragan73@gmail.com>
2023-11-12 12:27:10 +0100

    Añadir ejercicios-11-14

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+// Ejercicio 12: async/await en un bucle
+// Crea un bucle que realice llamadas asincrónicas utilizando async/await para procesar una lista de elementos uno por uno.
+async function procesarElementos(elementos) {
+    for (const elemento of elementos) {
+        await new Promise((resolve) =>
+            setTimeout(resolve, 1500))
+        console.log('Procesando elemento: ', elemento)
+    }
+}
+
+// Defino el número de Pronises que se ajecutarán
+const elementos = [1, 2, 3, 4]
+procesarElementos(elementos)
